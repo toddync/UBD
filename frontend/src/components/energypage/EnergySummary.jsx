e07117d6941b5eca1c31d7b976400707d9cc35bb
Marcos Souza <marcosfdesouza13@gmail.com>
2025-11-19 18:27:14 -0300

    grid energy

--- frontend/src/components/energypage/EnergySummary.jsx
+++ frontend/src/components/energypage/EnergySummary.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import EnergyEvolution from "./EnergyEvolution";
 
 export default function EnergySummary() {
   const [data, setData] = useState(null);
@@ -81,18 +82,27 @@ export default function EnergySummary() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {metrics.map((metric) => (
-          <MetricCard
-            key={metric.id}
-            title={metric.title}
-            value={metric.value}
-            color={metric.color}
-            icon={metric.icon}
-          />
-        ))}
-      </div>
+      <div className="energy__secondary">
+        <MetricContainer metrics={metrics}/>
+        <EnergyEvolution/>
+      </div>    
     </section>
+  );
+}
+
+function MetricContainer({metrics}) {
+  return(
+    <div className="energy__metrics">
+      {metrics.map((metric) => (
+        <MetricCard
+          key={metric.id}
+          title={metric.title}
+          value={metric.value}
+          color={metric.color}
+          icon={metric.icon}
+        />
+      ))}
+    </div>
   );
 }
 
