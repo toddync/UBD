9871006cb63363256ac0538e0ccebb7a6b35ecc8
DavyAndrade <davy.dama55@gmail.com>
2025-11-19 23:44:10 -0300

    feat: preparing components from health page

---
+++ frontend/src/components/healthpage/HealthMain.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HealthMain() {
+  return (
+    <div>HealthMain</div>
+  )
+}
---
+++ frontend/src/components/healthpage/HealthScatter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HealthScatter() {
+  return (
+    <div>HealthScatter</div>
+  )
+}
